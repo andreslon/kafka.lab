067a57091023a43052d2182e92d479a4e07e88a1
Andreslon <andreslon@outlook.com>
2023-06-11 22:21:49 -0500

    added .net example projects

---
+++ apps/net/kafka.consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092", // Dirección y puerto de los brokers de Kafka
+    GroupId = "my-consumer-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    string topic = "lab-topic";
+
+    consumer.Subscribe(topic);
+
+    CancellationTokenSource cts = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, e) =>
+    {
+        e.Cancel = true;
+        cts.Cancel();
+    };
+
+    try
+    {
+        while (true)
+        {
+            var consumeResult = consumer.Consume(cts.Token);
+            Console.WriteLine($"Mensaje recibido: {consumeResult.Message.Value}");
+        }
+    }
+    catch (OperationCanceledException)
+    {
+        // El consumidor se detuvo mediante una cancelación
+    }
+    finally
+    {
+        consumer.Close();
+    }
+}
---
+++ apps/net/kafka.producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092", // Dirección y puerto de los brokers de Kafka
+    ClientId = "my-producer"
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    string topic = "lab-topic";
+
+    for (int i = 0; i < 10; i++)
+    {
+        string message = $"Mensaje {i}";
+
+        var deliveryReport = producer.ProduceAsync(topic, new Message<Null, string>
+        {
+            Value = message
+        });
+
+        Console.WriteLine($"Mensaje enviado: {message}");
+
+        // Esperar a que se confirme el envío
+        var result = deliveryReport.GetAwaiter().GetResult();
+        Console.WriteLine($"Partición: {result.Partition}, Offset: {result.Offset}");
+    }
+
+    // Asegurarse de que todos los mensajes se envíen antes de cerrar el productor
+    producer.Flush(TimeSpan.FromSeconds(10));
+}
